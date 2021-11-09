8c45808f386c00f73e5ba605a1ce1c9ca99cdb39
Tiago manuel Ferreira nevoa <tferreir@c1r12s1.42lisboa.com>
2021-11-09 18:08:41 +0000

    vs2

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,11 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tferreir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/27 11:37:28 by tferreir          #+#    #+#             */
-/*   Updated: 2021/10/27 11:37:31 by tferreir         ###   ########.fr       */
+/*   Created: 2021/11/09 16:22:43 by tferreir          #+#    #+#             */
+/*   Updated: 2021/11/09 18:07:25 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*#include <stdio.h>*/
 
+int	ft_isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
+/*
+int	main(void)
+{
+	char	c;
+
+	c = 'a';
+	if (ft_isalpha(c))
+		printf("True");
+	else
+		printf("False");
+}
+*/

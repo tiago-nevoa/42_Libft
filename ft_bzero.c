f864bcd8b5786efcdca190b1dfdafd0857042ba0
Tiago manuel Ferreira nevoa <tferreir@c1r12s1.42lisboa.com>
2021-11-15 20:52:51 +0000

    v7.0

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/15 18:29:51 by tferreir          #+#    #+#             */
-/*   Updated: 2021/11/15 20:52:15 by tferreir         ###   ########.fr       */
+/*   Created: 2021/11/15 20:44:49 by tferreir          #+#    #+#             */
+/*   Updated: 2021/11/15 20:51:20 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*ptr;
-
-	ptr = b;
-	while (len-- > 0)
-		*ptr++ = c;
-	return (b);
+	ft_memset(s, 0, n);
 }

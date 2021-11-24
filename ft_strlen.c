fab77de33a84d9b9013fad420561f6c3b95eeac5
Tiago manuel Ferreira nevoa <tferreir@c1r21s9.42lisboa.com>
2021-11-24 18:54:40 +0000

    v10.6

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 18:39:26 by tferreir          #+#    #+#             */
-/*   Updated: 2021/11/10 19:31:43 by tferreir         ###   ########.fr       */
+/*   Updated: 2021/11/24 18:53:35 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,17 @@ size_t	ft_strlen(const char *s)
 	size_t	index;
 
 	index = 0;
-	while (s[index] != '\0')
-	{
+	while (s[index])
 		index++;
-	}
 	return (index);
 }
+/*
+int	main(void)
+{
+	char	*s;
+
+	s = "123456";
+	printf("The String s = %s\n", s);
+	printf("s lenght is = %zu", ft_strlen(s));
+}
+*/

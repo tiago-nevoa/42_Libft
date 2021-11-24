507c6338106a211f9a00534c8036d59518651310
Tiago manuel Ferreira nevoa <tferreir@c1r21s9.42lisboa.com>
2021-11-24 19:25:20 +0000

    v10.7

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 18:29:51 by tferreir          #+#    #+#             */
-/*   Updated: 2021/11/15 20:52:15 by tferreir         ###   ########.fr       */
+/*   Updated: 2021/11/24 19:24:38 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,12 @@ void	*ft_memset(void *b, int c, size_t len)
 		*ptr++ = c;
 	return (b);
 }
+/*
+int	main(void)
+{
+	char	b[50] = "abdzA62~";
+
+	printf("The String b = %s\n", b);
+	printf("The String b = %s\n", ft_memset(b, '@', 2));
+}
+*/

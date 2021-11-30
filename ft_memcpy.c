8b95d74d728699373da2f224dbc0264e7045756b
Tiago manuel Ferreira nevoa <tferreir@c1r21s9.42lisboa.com>
2021-11-30 16:49:39 +0000

    v10.8

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 20:58:02 by tferreir          #+#    #+#             */
-/*   Updated: 2021/11/24 16:37:35 by tferreir         ###   ########.fr       */
+/*   Updated: 2021/11/30 16:48:35 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+	char		*d;
+	const char	*s;
 
+	d = dst;
+	s = src;
+	while (n-- > 0)
+		*d++ = *s++;
+	return (dst);
 }
+/*
+int	main(void)
+{
+	const char	src[50] = "0123456789";
+	char		dst[50] = "9876543210";
+
+	printf("Before memcpy dest = %s\n", dst);
+	printf("After memcpy dest = %s\n", memcpy(dst, src, 5));
+}
+*/

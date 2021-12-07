176b1431823607f823a5d2dabf3bf575432e138e
Tiago manuel Ferreira nevoa <tferreir@c1r21s7.42lisboa.com>
2021-12-07 12:35:16 +0000

    v11.6

--- Libft_Code/ft_strchr.c
+++ Libft_Code/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 17:54:25 by tferreir          #+#    #+#             */
-/*   Updated: 2021/12/02 20:46:31 by tferreir         ###   ########.fr       */
+/*   Updated: 2021/12/07 12:31:14 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (s++)
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (*s == c)
-			return ((char *)s);
-		if (*s == '\0')
-			return (NULL);
+		if (s[i] == c)
+			return ((char *)(s + i));
+		i++;
 	}
+	if (c == '\0')
+		return ((char *)(s + i));
 	return (NULL);
 }
 /*
@@ -29,8 +33,8 @@ int	main(void)
 	const char	src[20] = "0123456789";
 
 	printf("Before strchr dest = %s\n", src);
-	printf("After ft_strchr dest = %s\n", ft_strchr(src, '\0'));
-	printf("After strchr dest = %s\n", strchr(src, '\0'));
+	printf("After ft_strchr dest = %s\n", ft_strchr(src, '0'));
+	printf("After strchr dest = %s\n", strchr(src, '0'));
 	return (0);
 }
 */

d883466173aa362093657f2c41ef2912c8f61d30
Tiago manuel Ferreira nevoa <tferreir@c1r21s7.42lisboa.com>
2021-12-13 18:37:42 +0000

    v12.5

--- Libft_Code/ft_strlcat.c
+++ Libft_Code/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 20:08:18 by tferreir          #+#    #+#             */
-/*   Updated: 2021/12/10 20:58:13 by tferreir         ###   ########.fr       */
+/*   Updated: 2021/12/13 18:36:44 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,36 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	dstlen;
-	size_t	srclen;
 	size_t	size;
+	size_t	maxlen;
 
-	dstlen = ft_strlen(dst);
-	srclen = ft_strlen(src);
 	size = dstsize;
-	if (dstlen == size)
-		return (srclen + size);
-	size += dstlen;
-	while (src && size > 1)
+	maxlen = ft_strlen(dst)+ ft_strlen(src);
+	while (*dst && size > 0)
 	{
-		dst[dstlen++] = *src++;
+		dst++;
 		size--;
 	}
-	dst[dstlen] = '\0';
-	return (dstlen);
+	if (size == 0)
+		return (ft_strlen(src) + dstsize);
+	while (*src && size > 1)
+	{
+		*dst++ = *src++;
+		size--;
+	}
+	*dst = 0;
+	return (maxlen);
+}
+
+int	main(void)
+{
+	char			dst[30] = "Amazing";
+	const char		src[5] = "Tits ";
+	const char		src1[20] = "&& Round Ass";
+	size_t			n;
+
+	n = ft_strlcat(dst, src, 30);
+	printf("ft_strlcat	<%s> : %zu \n", dst, n);
+	n = strlcat(dst, src1, 20);
+	printf("strlcat		<%s> : %zu \n", dst, n);
 }

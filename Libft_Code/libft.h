dcfe3c4a5d35c6d1b7dbe3b219be4afd16df253c
Tiago manuel Ferreira nevoa <tferreir@c1r21s7.42lisboa.com>
2021-12-16 13:35:11 +0000

    12.9

--- Libft_Code/libft.h
+++ Libft_Code/libft.h
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:20:23 by tferreir          #+#    #+#             */
-/*   Updated: 2021/12/15 20:14:10 by tferreir         ###   ########.fr       */
+/*   Updated: 2021/12/16 13:15:00 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif

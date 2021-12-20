de95b4aac4b3bb8a53865d4f9853b9f01ab7b526
Tiago manuel Ferreira nevoa <tferreir@c1r21s7.42lisboa.com>
2021-12-20 12:30:20 +0000

    v13.1

--- Libft_Code/libft.h
+++ Libft_Code/libft.h
@@ -6,7 +6,7 @@
 /*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:20:23 by tferreir          #+#    #+#             */
-/*   Updated: 2021/12/16 16:50:58 by tferreir         ###   ########.fr       */
+/*   Updated: 2021/12/20 12:29:22 by tferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	*ft_strdup(const char *s1);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
